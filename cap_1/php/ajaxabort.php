81a92abd3fec07353e1652bac4fbe8a9ce014b9c
Armando Couto <armando-couto@hotmail.com>
2013-03-20 22:23:15 -0300

    Concluido parte do capitulo 1.

---
+++ cap_1/php/ajaxabort.php
@@ -0,0 +1,13 @@
+﻿<?php
+  $gmtDate  = gmdate("D, d M Y H:i:s");
+  header("Expires: {$gmtDate} GMT");
+  header("Last-Modified: {$gmtDate} GMT");
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Pragma: no-cache");
+  header("Content-Type: text/html; charset=utf-8");
+  if(isset($_GET["abortar"])) {
+  sleep(7000); /* Nota importante: A fun&ccedil;&atilde;o sleep() foi incluida com fins did&aacute;ticos, somente para mostrar uma requisi&ccedil;&atilde;o abortada.*/  
+   
+  echo ("<p>Este &eacute; o conte&uacute;do requisitado para demonstrar o m&eacute;todo abort()</p>");
+  } 
+?>
---
+++ cap_1/php/requisicao-php.php
@@ -0,0 +1,14 @@
+﻿<?php
+  $gmtDate  = gmdate("D, d M Y H:i:s");
+  header("Expires: {$gmtDate} GMT");
+  header("Last-Modified: {$gmtDate} GMT");
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Pragma: no-cache");
+  header("Content-Type: text/html; charset=utf-8");
+  
+  if(isset($_GET["insere"])) {
+  sleep(4); /* Nota importante: A fun&ccedil;&atilde;o sleep() foi incluida com fins did&aacute;ticos, somente para mostrar o &iacute;cone de               carregamento do AJAX.*/  
+  echo ("<p>Este &eacute; o conte&uacute;do requisitado para demonstrar a inser&ccedil;&atilde;o de uma imagem animada indicativa de carregamento.<p>
+         <p>Usamos a imagem de um &aacute;tomo gerada no site <a href= 'http://www.preloaders.net/en/3d'>         http://www.preloaders.net/en/3d</a></p>");
+  }
+?>
